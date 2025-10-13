26fe63822cf085e0c5db53c753243ba0d307cb52
paleon08 <paleon08@naver.com>
2025-10-13 13:11:33 +0900

    del2

--- tuc/ops/features/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from pathlib import Path
-import numpy as np
-
-class BaseAudioEmbedder(ABC):
-    @abstractmethod
-    def __call__(self, wav_path: Path) -> np.ndarray:
-        ...
--- tuc/transforms/base.py
+++
@@ -1,5 +0,0 @@
-from abc import ABC, abstractmethod
-import numpy as np
-class BaseTransform(ABC):
-    @abstractmethod
-    def __call__(self, **kwargs) -> np.ndarray: ...
